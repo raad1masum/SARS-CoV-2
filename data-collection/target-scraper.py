b705a0694cac36f44adb360722aed2b588ef5f93
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-03-26 15:51:19 -0700

    Create target-scraper.py

---
+++ data-collection/target-scraper.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+url = input("Target URL: ")
+
+my_url = url
+
+# Open connection and grabs page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parser
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+containers = page_soup.findAll("div",{"class":"ProductCardBody-n7791b-0 styles__StyledProductCardBody-e5kry1-3 mIJeb"})
+
+for container in containers:
+    product_container = container.findAll("a", {"class": "Link-sc-1khjl8b-0 styles__StyledTitleLink-e5kry1-5 cPukFm h-display-block h-text-bold h-text-bs flex-grow-one"})
+    price_container = container.findAll("span", {"class": "h-text-bs"})
+    if len(price_container) > 0:
+        price = price_container[0].span.text.strip()
+        product = product_container[0].span.text.strip()
+
+    print("Product: " + product)
+    print("Price: " + price)
+    print("")
