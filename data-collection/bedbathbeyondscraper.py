ccb513b3e6576f346c122406ba94ba9c6ff1f139
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-03-26 19:34:53 -0700

    bbab

---
+++ data-collection/bedbathbeyondscraper.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+url = input("BedBath and Beyond URL: ")
+
+my_url = url
+
+# Open connection and grabs page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parser
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+containers = page_soup.findAll("div",{"class":"ProductGrid-inline_7ebuPJ ProductGrid-inline_6uaOga"})
+
+for container in containers:
+    #product_container = container.findAll("a", {"href": "PrimaryLink_1RLwvm inline-block"})
+    price_container = container.findAll("span", {"class": "Price_3HnIBb block"})
+    if len(price_container) > 0:
+        price = price_container[0].span.text.strip()
+        product = product_container[0].span.text.strip()
+
+    print("Product: " + product)
+    print("Price: " + price)
+    print("")
